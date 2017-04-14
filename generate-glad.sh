258579f668a03bab24a6b19828efc56e01359653
Fred Sundvik <fsundvik@gmail.com>
2017-04-14 19:31:04 +0300

    Add glad for OpenGL support
    That includes a script for generating the folder

---
+++ generate-glad.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# You need to install glad to use this script
+# https://github.com/Dav1dde/glad
+
+glad --out-path="glad" --profile="compatibility" --api="gl=3.3" --generator="c" --spec="gl" --no-loader --extensions=""
