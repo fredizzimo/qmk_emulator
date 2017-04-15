38af350bd6fea5b424b5c5eb42171982d5d26025
Fred Sundvik <fsundvik@gmail.com>
2017-04-15 08:39:51 +0300

    Move glad into lib directory

--- generate-glad.sh
+++ generate-glad.sh
@@ -2,4 +2,4 @@
 # You need to install glad to use this script
 # https://github.com/Dav1dde/glad
 
-glad --out-path="glad" --profile="compatibility" --api="gl=3.3" --generator="c" --spec="gl" --no-loader --extensions=""
+glad --out-path="lib/glad" --profile="compatibility" --api="gl=3.3" --generator="c" --spec="gl" --no-loader --extensions=""
