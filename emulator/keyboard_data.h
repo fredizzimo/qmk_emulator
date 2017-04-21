4eb1aca9df68be1330853d31f5577933654995c0
Fred Sundvik <fsundvik@gmail.com>
2017-04-22 01:58:57 +0300

    Fix the shape of the keyboard vertex data

--- emulator/keyboard_data.h
+++ emulator/keyboard_data.h
@@ -41,6 +41,7 @@ static const float lcd_alpha = 0.85f;
 static const color_t lcd_black_color = RGB2COLOR(0, 0, 0);
 
 static const GLfloat keyboard_vertex_data[] = {
+	300, 300,
     25, 0,
     210, 0,
     242, 7,
@@ -58,6 +59,7 @@ static const GLfloat keyboard_vertex_data[] = {
     25, 543,
     0, 517,
     0, 25,
+	25, 0,
 };
 
 static const float keycap_size = 73.66f; // 7.25 * 2.54 * 4
